f05a0a18868c1c3be070d9e1e2f694f4bafcafa7
prapinporn4011 <66010914011@msu.ac.th>
2026-02-17 14:18:14 +0700

    a

--- login_db.php
+++ login_db.php
@@ -1,25 +1,22 @@
 <?php
 session_start();
 include('db.php');
-
 if (isset($_POST['login'])) {
     $user = mysqli_real_escape_string($conn, $_POST['username']);
     $pass = $_POST['password'];
 
     $sql = "SELECT * FROM users WHERE username = '$user'";
     $result = mysqli_query($conn, $sql);
-
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        // ตรวจสอบรหัสผ่านที่เข้ารหัสไว้
         if (password_verify($pass, $row['password'])) {
             $_SESSION['username'] = $row['username'];
-            header("Location: shop.php"); // เข้าสู่ระบบสำเร็จ ไปหน้า shop.php
+            header("Location: store.php"); // เมื่อผ่านให้กลับไปหน้าเดิมแต่เปลี่ยนสถานะ
         } else {
             echo "<script>alert('รหัสผ่านไม่ถูกต้อง'); window.history.back();</script>";
         }
     } else {
-        echo "<script>alert('ไม่พบชื่อผู้ใช้งานนี้'); window.history.back();</script>";
+        echo "<script>alert('ไม่พบชื่อผู้ใช้นี้'); window.history.back();</script>";
     }
 }
 ?>
