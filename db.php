89cffe78698338450055ffa5d5bec741e948419f
prapinporn4011 <66010914011@msu.ac.th>
2026-03-09 22:54:02 +0700

    a

--- db.php
+++ db.php
@@ -1,12 +1,12 @@
 <?php
 $host = 'localhost';
-$dbname = 'shop1'; // อ้างอิงชื่อฐานข้อมูลจากรูปของคุณ
-$user = 'root'; // ชื่อผู้ใช้เริ่มต้นของ XAMPP
-$pass = ''; // รหัสผ่านเริ่มต้นของ XAMPP (ปล่อยว่างไว้)
+$dbname = 'shop1'; // ชื่อฐานข้อมูลตามในรูปของคุณ
+$username = 'root'; // <-- ตรงนี้อาจจะต้องเปลี่ยนเป็น user ที่คุณสร้างไว้
+$password = 'ใส่รหัสผ่านฐานข้อมูลของคุณที่นี่'; // <-- ต้องใส่รหัสผ่านให้ถูกต้อง
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    // ตั้งค่าให้แสดง Error หากมีปัญหา
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // ตั้งค่า Error Mode
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     die("การเชื่อมต่อฐานข้อมูลล้มเหลว: " . $e->getMessage());
